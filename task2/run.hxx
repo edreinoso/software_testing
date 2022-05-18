7b9546a7827db8151e72e8a39453c4308d98c1da
Ed Reinoso <edgardojesus16@gmail.com>
2022-05-18 17:25:19 +0200

    latest changes for the neville car, mainly avoiding obstacles and turning LED light on/off

--- task2/run.hxx
+++ task2/run.hxx
@@ -2,6 +2,9 @@
 #include "physcom.h" 
 using namespace physcom;
 Serial pc(USBTX, USBRX); // open a serial communication to terminal emulator
+AnalogIn passive_light(p18);
+DigitalOut redLed(p20);
+Ping Pinger(p11);
 M3pi robot; //create an object of type M3pi
 
 void set_speed(float left, float right) {
@@ -15,12 +18,26 @@ int main() {
     robot.sensor_auto_calibrate(); //robot executes calibration
     pc.printf("Finished calibration!\r\n");
     float min_black_value = 700; // assume something
-    
+    float light_value;
+
     while (1) { // while loop to move the sensor around until an edge case
+        light_value = passive_light.read(); 
         int sensors[5]; // array init sensors
         robot.calibrated_sensors(sensors); //5 values are read in a vector
         pc.printf("calibrating light sensor\r\n");
         pc.printf("outside sensor values: %d; %d; %d; %d; %d;\r\n", sensors[0], sensors[1],sensors[2], sensors[3], sensors[4]);
+
+        // If something is close enough, we should break out of the loop
+        int range;
+        Pinger.Send();
+        wait_ms(5);
+        range = Pinger.Read_cm(); 
+        pc.printf("range: %d\r\n", range);
+        if (range < 9 && range > 0) {
+            set_speed(0, 0);
+            continue;
+        }
+        
         int current_sensor = 0;
         for (int i = 0; i < 5; i++) {
             if (sensors[i] > sensors[current_sensor]) {
@@ -36,32 +53,41 @@ int main() {
             set_speed(0.0, 0.0);
             break;
         }
-        else if (sensors[0] > 700 && sensors[1] > 700) { // right turn
-            current_sensor = -1;
-            set_speed(-0.3, 0.3);
-        }
-        else if (sensors[3] > 700 && sensors[4] > 700) { // right turn
-            current_sensor = -1;
-            set_speed(0.3, -0.3);
-        }
+//        else if (sensors[0] > 700 && sensors[1] > 700) { // right turn
+//            current_sensor = -1;
+//            set_speed(-0.3, 0.3);
+//        }
+//        else if (sensors[3] > 700 && sensors[4] > 700) { // right turn
+//            current_sensor = -1;
+//            set_speed(0.3, -0.3);
+//        }
         
         // left most, turn left
         if (current_sensor == 0) {
-            set_speed(0, 0.3);
+            set_speed(-0.05, 0.2);
         }
         else if (current_sensor == 1) {
-            set_speed(0.15, 0.2);
+            set_speed(0.05, 0.15);
         }
         else if (current_sensor == 2) {
-            set_speed(0.2, 0.2);
+            set_speed(0.15, 0.15);
         }
         else if (current_sensor == 3) {
-            set_speed(0.2, 0.15);
+            set_speed(0.15, 0.05);
         }
         else if (current_sensor == 4) {
-            set_speed(0.3, 0);
+            set_speed(0.2, -0.05);
         }
         pc.printf("\n");
+        pc.printf("value: %f\r\n", light_value);
+        if (light_value > 0.35) {
+            // Turn on the LED light
+            redLed = 1;
+            pc.printf("hello world\r\n");
+        } else {
+            redLed = 0;
+        }
+
     } 
     pc.printf("motor stopped\r\n");
 }
